4a5b15c5ef1d3621892992d24aa67ae29227fc56
Mingjia Shi <46616863+baieva@users.noreply.github.com>
2019-06-28 21:12:16 -0400

    Add files via upload

---
+++ eccca_p01/script/contact_me.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+   // data sent in header are in JSON format
+   header('Content-Type: application/json');
+   // takes the value from variables and Post the data
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $postmessage = $_POST['message'];
+   $to = "bruceyiuri@gmail.com";
+   $subject = "Contact Us";
+   // Email Template
+   $message = "<b>Name : </b>". $name ."<br>";
+   $message .= "<b>Email Address : </b>".$email."<br>";
+   $message .= "<b>Message : </b>".$postmessage."<br>";
+
+   $header = "From:".$email;
+   //$header = "From:"+$email+" \r\n";
+   //$header .= "MIME-Version: 1.0\r\n";
+   //$header .= "Content-type: text/html\r\n";
+   echo $to;
+   echo $subject;
+   echo $message;
+   echo $header;
+   $retval = mail($to,$subject,$message,$header);
+   // message Notification
+   if( $retval == true ) {
+      echo json_encode(array(
+         'success'=> true,
+         'message' => 'Message sent successfully'
+      ));
+   }else {
+      echo json_encode(array(
+         'error'=> true,
+         'message' => 'Error sending message'
+      ));
+   }
+?>
+
+</body>
+</html>
